7fbb15e9b40e80f86ff3a90ad751f4442693c517
Raymond Cox <rj.cox101@gmail.com>
2012-05-28 18:16:41 -0400

    Added server.properties

--- src/com/openorpg/simpleorpg/server/Server.java
+++ src/com/openorpg/simpleorpg/server/Server.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
 import com.openorpg.simpleorpg.server.net.JoinGameHandler;
 import com.openorpg.simpleorpg.server.net.LeaveGameHandler;
 import com.openorpg.simpleorpg.server.net.MessageHandler;
+import com.openorpg.simpleorpg.shared.PropertiesLoader;
 
 public class Server {
 	private static final Logger logger = Logger.getLogger(Server.class);
@@ -19,7 +21,9 @@ public class Server {
 	 */
 	public static void main(String[] args) {
 		final ServerSocket serverSocket;
-		int port = 1234;
+		PropertiesLoader propertiesLoader = PropertiesLoader.getInstance();
+		final Properties serverProperties = propertiesLoader.load("server.properties");
+		int port = Integer.valueOf(serverProperties.getProperty("port", "1234"));
 		
 		MessageHandler.init();
 		
